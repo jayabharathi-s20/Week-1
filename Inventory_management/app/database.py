d0149e16853165fec1336bcbb0a44b0af0b8035e
jayabharathi-s20 <jayabharathis@bitcot.com>
2026-05-05 15:47:37 +0530

    merged connection.py and model.py

--- Inventory_management/app/database.py
+++ Inventory_management/app/database.py
@@ -1,18 +1,29 @@
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-db_user=os.getenv("db_user")
-db_pw=os.getenv("db_pw")
-db_host=os.getenv("db_host")
-db_port=os.getenv("db_port")
-db_name=os.getenv("db_name")
 
-database_url=f"postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}"
-engine=create_engine(database_url)
+db_user = os.getenv("db_user")
+db_pw = os.getenv("db_pw")
+db_host = os.getenv("db_host")
+db_port = os.getenv("db_port")
+db_name = os.getenv("db_name")
 
-SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
-Base=declarative_base()
+DATABASE_URL = f"postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}"
 
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def init_db():
+    import app.models  
+    Base.metadata.create_all(bind=engine)
